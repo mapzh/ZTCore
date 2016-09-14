be929aecf55b6eb1481fe7ee1dfddd116a1894f4
mapengzhen <mapengzhen@rd.tuan800.com>
2016-09-14 11:17:02 +0800

    c++/c

--- ZTCore/source/function/ZTCommonFunction.h
+++ ZTCore/source/function/ZTCommonFunction.h
@@ -8,6 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
+#if defined __cplusplus
+extern "C" {
+#endif
+
 //判断是否空数组
 BOOL isNotEmptyArray(NSArray *arr);
 
@@ -16,3 +20,8 @@ BOOL isNotEmptyDictionary(NSDictionary *dict);
 
 //判断是否空字符串
 BOOL isNotEmptyString(NSString *str);
+
+
+#if defined __cplusplus
+};
+#endif
